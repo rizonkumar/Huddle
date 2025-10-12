353751ccc8826a035b32ac185035db85a2930ffe
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-10-12 19:42:42 +0530

    Refactor marketing page structure by removing the old Home component and introducing a new layout with dedicated header and hero section components. Add logo and enhance UI components for better branding and user experience.

--- app/(marketing)/_components/header.tsx
+++ app/(marketing)/_components/header.tsx
@@ -1,9 +1,9 @@
 "use client";
 import Link from "next/link";
-import { Logo } from "@/components/logo";
 import { Menu, X } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Logo } from "@/app/(marketing)/_components/logo";
 import React from "react";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 const menuItems = [
